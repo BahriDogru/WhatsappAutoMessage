98821a00b63b19b070e386d4d1c0e26d1051e78f
Bahri DOĞRU <93467768+BahriDogru@users.noreply.github.com>
2023-04-20 00:39:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ phone_number ="+905019031903"
 when = input("Mesajı göndermek istediğiniz saat ve dakika(örnek:19:03): ")
 hour, munite = when.split(":")
 
-with open("messages.txt", "r", encoding="UTF-8") as f:
+with open("MassegeFile.txt", "r", encoding="UTF-8") as f:
     msg = f.readlines()
     random.shuffle(msg)
     message = random.choice(msg)
